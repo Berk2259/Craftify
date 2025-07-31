c027bf1af10acdb2d5714202cf9baa36579a20ee
Berk Altay <berkaltay3435@gmail.com>
2025-07-31 18:13:58 +0300

    container denemesi

--- lib/Screen/HomeScreen.dart
+++ lib/Screen/HomeScreen.dart
@@ -12,6 +12,47 @@ class HomeScreen extends StatelessWidget {
         title: Text('Ana Sayfa', style: TextStyle(color: Colors.white)),
       ),
       backgroundColor: Colors.deepPurple.shade300,
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              height: 350,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white70,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 16.0, left: 40),
+                        child: Image(
+                          image: AssetImage('assets/images/paper1.png'),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 16.0, left: 8.0),
+                        child: Text(
+                          'Gül Yapımı',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 15),
+                  Image(image: AssetImage('assets/images/indir.jpeg')),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
