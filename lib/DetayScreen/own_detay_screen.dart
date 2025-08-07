4e5f6ed02c537de6d12179ae4ebb6f357cbfc5a1
Berk Altay <berkaltay3435@gmail.com>
2025-08-07 14:18:15 +0300

    ownDetayScreen kısmı yapısal düzenleme

--- lib/DetayScreen/own_detay_screen.dart
+++ lib/DetayScreen/own_detay_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class OwnDetayScreen extends StatefulWidget {
   const OwnDetayScreen({super.key});
@@ -8,12 +9,36 @@ class OwnDetayScreen extends StatefulWidget {
 }
 
 class _OwnDetayScreenState extends State<OwnDetayScreen> {
+  String? secilenDeger;
+  List<String> secenekler = ['Uhu', 'Bant', 'Makas', 'Cetvel', 'İğne', 'Kağıt'];
+  List<String> liste = [];
+
+  Widget getIconForOption(String option) {
+    switch (option) {
+      case 'Uhu':
+        return Image.asset('assets/images/glue.png', width: 20, height: 20);
+      case 'Bant':
+        return Image.asset('assets/images/tape.png', width: 20, height: 20);
+      case 'Makas':
+        return Image.asset('assets/images/scissor.png', width: 20, height: 20);
+      case 'Cetvel':
+        return Image.asset('assets/images/ruler.png', width: 20, height: 20);
+      case 'İğne':
+        return Image.asset('assets/images/needle.png', width: 20, height: 20);
+      case 'Kağıt':
+        return Image.asset('assets/images/paper1.png', width: 20, height: 20);
+      default:
+        // Tanımlanmayan seçenekler için varsayılan simge
+        return Icon(Icons.help_outline, size: 20, color: Colors.grey);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurple.shade300,
       appBar: AppBar(
-        title: Text('Kendin Ekle', style: TextStyle(color: Colors.white)),
+        title: const Text('Kendin Ekle', style: TextStyle(color: Colors.white)),
         backgroundColor: Colors.deepPurple.shade900,
         leading: const BackButton(color: Colors.white),
         centerTitle: true,
@@ -24,13 +49,190 @@ class _OwnDetayScreenState extends State<OwnDetayScreen> {
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: Container(
-                width: double.infinity,
-                height: MediaQuery.of(context).size.height,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(15),
                   color: Colors.white70,
                 ),
-                child: Column(children: [],),
+                child: Column(
+                  children: [
+                    // Üst Bilgi Kutusu
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Container(
+                        padding: const EdgeInsets.all(16.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.blue,
+                        ),
+                        child: Row(
+                          children: const [
+                            Icon(
+                              FontAwesomeIcons.circleInfo,
+                              color: Colors.white,
+                            ),
+                            SizedBox(width: 10),
+                            Text(
+                              'Kendin Ekle',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+
+                    // İçerik Kutusu
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: Colors.blue,
+                          ),
+                          child: Column(
+                            children: [
+                              // TextField
+                              Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: TextField(
+                                  cursorColor: Colors.white,
+                                  style: const TextStyle(color: Colors.white),
+                                  decoration: const InputDecoration(
+                                    labelText: 'İsim Girin',
+                                    labelStyle: TextStyle(color: Colors.white),
+                                    enabledBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                    focusedBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                        color: Colors.white,
+                                        width: 2.0,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+
+                              // Dropdown + Ekle Butonu
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 16.0),
+                                    child: DropdownButton<String>(
+                                      dropdownColor: Colors.white,
+                                      hint: const Text('Bir seçenek seçin'),
+                                      value: secilenDeger,
+                                      onChanged: (String? newValue) {
+                                        setState(() {
+                                          secilenDeger = newValue;
+                                        });
+                                      },
+                                      items:
+                                          secenekler.map((String secenek) {
+                                            return DropdownMenuItem<String>(
+                                              value: secenek,
+                                              child: Row(
+                                                children: [
+                                                  getIconForOption(secenek),
+                                                  const SizedBox(width: 8),
+                                                  Text(secenek),
+                                                ],
+                                              ),
+                                            );
+                                          }).toList(),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(right: 16.0),
+                                    child: ElevatedButton(
+                                      onPressed: () {
+                                        if (secilenDeger != null &&
+                                            !liste.contains(secilenDeger)) {
+                                          setState(() {
+                                            liste.add(secilenDeger!);
+                                          });
+                                        }
+                                      },
+                                      child: const Text('Listeye Ekle'),
+                                    ),
+                                  ),
+                                ],
+                              ),
+
+                              const SizedBox(height: 10),
+
+                              // Liste
+                              Expanded(
+                                child: ListView.builder(
+                                  itemCount: liste.length,
+                                  itemBuilder: (context, index) {
+                                    String item = liste[index];
+                                    return Padding(
+                                      padding: const EdgeInsets.only(
+                                        top: 16.0,
+                                        left: 16.0,
+                                        right: 16.0,
+                                      ),
+                                      child: Container(
+                                        width: double.infinity,
+                                        height: 60,
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(
+                                            15,
+                                          ),
+                                          color: Colors.white,
+                                        ),
+                                        child: Center(
+                                          child: ListTile(
+                                            leading: getIconForOption(item),
+                                            title: Text(
+                                              item,
+                                              style: const TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 18,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ),
+
+                              // Kaydet Butonu
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: ElevatedButton.icon(
+                                  onPressed: () {
+                                    // Kaydetme işlemi buraya yazılabilir
+                                  },
+                                  icon: const Icon(
+                                    Icons.save_rounded,
+                                    size: 25,
+                                  ),
+                                  label: const Text(
+                                    'Kaydet',
+                                    style: TextStyle(fontSize: 18),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
