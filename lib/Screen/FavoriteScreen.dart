67e6b8b08b6e0007e76b91d4818c516fc242079c
Berk Altay <berkaltay3435@gmail.com>
2025-08-07 12:09:07 +0300

    favori ekranı maskot eklenmesi

--- lib/Screen/FavoriteScreen.dart
+++ lib/Screen/FavoriteScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:craftify/model/favorite_item_model.dart';
+import 'package:craftify/Widget/widget.dart';
 
 class FavoriteScreen extends StatelessWidget {
   const FavoriteScreen({super.key});
@@ -15,23 +16,48 @@ class FavoriteScreen extends StatelessWidget {
         centerTitle: true,
         title: Text('Favori Sayfası', style: TextStyle(color: Colors.white)),
       ),
-      body: favorites.isEmpty
-          ? Center(child: Text('Henüz favori eklenmedi.', style: TextStyle(color: Colors.white, fontSize: 18)))
-          : ListView.builder(
-              itemCount: favorites.length,
-              itemBuilder: (context, index) {
-                final item = favorites[index];
-                return Card(
-                  margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      backgroundImage: AssetImage(item.imagePath),
+      body:
+          favorites.isEmpty
+              ? Center(
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Center(
+                        child: Stack(
+                          alignment: Alignment.topCenter,
+                          clipBehavior: Clip.none,
+                          children: [
+                            Positioned(
+                              top: -80,
+                              child: ThoughtBubble(
+                                text:
+                                    "Hey sen! Ben Mercan buranın efendisi benim \nsen yeni bir şey favorileyene kadar \nsana ben eşlik edeceğim",
+                              ),
+                            ),
+                            Image.asset('assets/images/favori_maskot.png'),
+                          ],
+                        ),
+                      ),
                     ),
-                    title: Text(item.title),
-                  ),
-                );
-              },
-            ),
+                    SizedBox(height: 20),
+                  ],
+                ),
+              )
+              : ListView.builder(
+                itemCount: favorites.length,
+                itemBuilder: (context, index) {
+                  final item = favorites[index];
+                  return Card(
+                    margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: AssetImage(item.imagePath),
+                      ),
+                      title: Text(item.title),
+                    ),
+                  );
+                },
+              ),
     );
   }
 }
